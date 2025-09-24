95c683e9b48847a5f94f99751ee2cd831820a474
walidhbabou <161231266+walidhbabou@users.noreply.github.com>
2025-09-24 16:12:23 +0200

    ff

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  optimizeDeps: {
+  include: ["buffer", "process", "util"]
+  },
   plugins: [
     react(),
     mode === 'development' &&
